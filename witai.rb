6e28d30946a8c7812c0999735c0623fbf93a9b72
aaronnke <aaronnke@gmail.com>
2017-06-20 17:44:43 +0800

    added joke function

--- witai.rb
+++ witai.rb
@@ -21,7 +21,6 @@ entities = rsp['entities']
 intent = entities['intent'][0]['value'] if entities['intent']
 date_time = entities['datetime'] ? entities['datetime'][0]['value'].slice(0,19) : nil
 location = entities['location'][0]['value'].upcase if entities['location']
-
 reply = nil
 case intent
 when 'air_temperature'
@@ -39,6 +38,17 @@ when 'weather'
   index = data['forecasts'].index{ |loc| loc['area'].upcase == location }
   forecast = data['forecasts'][index]['forecast']
   reply = "The forecast for #{location} between #{start_time} and #{end_time} is #{forecast}"
+when 'joke'
+  joke_type = entities['joke_type'] ? entities['joke_type'][0]['value'] : 'lame'
+  contact = entities['contact'] ? entities['contact'][0]['value'] : 'you'
+  joke = nil
+  case joke_type
+  when 'lame'
+    joke = "What is Transformer's sister called? Transistor!"
+  when 'sad'
+    joke = "There once was a frog who was so ugly everyone died."
+  end
+  reply = "Hey #{contact}, #{joke}."
 end
 
 puts reply
